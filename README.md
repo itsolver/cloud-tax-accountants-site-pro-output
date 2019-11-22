c40e528db6c36d43d53b9e33a9d28126bb039681
CloudCannon <support@cloudcannon.com>
2019-11-22 00:56:11 +0000

    Updated 80 files via CloudCannon

--- README.md
+++
@@ -1,1 +0,0 @@
-# cloud-tax-accountants-site-pro-output
---
+++ assets/js/pages/home.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $('.slider').slick({
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+  });
+});
